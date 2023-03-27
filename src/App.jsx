5da5bee8820a405d1b005dc46e45f844775b7528
deborasous <deborapatriciadp27@gmail.com>
2023-03-27 13:21:42 -0300

    [M1S08] Ex 3 - Criar useState para armazenar lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import CardAdd from "./components/CardAdd";
 
 function App() {
+  const [task, setTask] = useState([]);
+  
   function createTask() {}
 
   return (
